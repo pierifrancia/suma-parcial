25108346e02a9a6c5554c6ee9ed94d32293f365a
pierifrancia  <pieri.francia@gmail.com>
2019-11-08 22:15:54 -0300

    ejercicio 6

--- sumaparcial.js
+++ sumaparcial.js
@@ -1,11 +1,22 @@
-let suma = 0;
+// let suma = 0;
 
-for (let i = 0; i < 1001; i++) {
+// for (let i = 0; i < 1001; i++) {
 
-let resultado = suma + i;
+// let resultado = suma + i;
 
-suma = resultado;
+// suma = resultado;
 
-console.log (resultado);
+// console.log (resultado);
 
+// }
+
+let suma = 0
+
+for (let i = 1; i < 1001; i=i+2) {
+ 
+    let resultado = suma + i
+ 
+    suma = resultado
+
+    console.log (suma);
 }
